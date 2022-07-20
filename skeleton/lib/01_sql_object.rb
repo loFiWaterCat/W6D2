af06f5ca3d5d2ae566d953e66ba01d1308827272
jefzli21 <jefzli21@gmail.com>
2022-07-20 09:06:35 -0700

    sql

--- skeleton/lib/01_sql_object.rb
+++ skeleton/lib/01_sql_object.rb
@@ -1,5 +1,6 @@
 require_relative 'db_connection'
 require 'active_support/inflector'
+
 # NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
 # of this project. It was only a warm up.
 
@@ -12,15 +13,39 @@ class SQLObject
   end
 
   def self.table_name=(table_name)
-    # ...
+    # define_method("table_name=") do 
+    #   instance_variables_set(@table_name, table_name)
+    # end
+    # define_method("table_name=") do |table_name|
+    #   @table_name = table_name
+    self.class.table_name = table_name
+    
   end
 
   def self.table_name
-    # ...
+    # self.to_s.downcase + "s"
+    # str1 = self.to_s
+    # str2 = ""
+    # str1.each_char.with_index do |char, i|
+    #   if char == char.upcase && i != 0
+    #       str2 += "_" + char.downcase
+    #   else
+    #     str2 += char.downcase
+    #   end
+    # end
+    # return str2 += "s"
+    self.to_s.tableize
   end
 
   def self.all
     # ...
+    # execute(<<-SQL)
+    # SELECT
+    # *
+    # FROM
+    # self.table_name;
+
+    # SQL
   end
 
   def self.parse_all(results)
